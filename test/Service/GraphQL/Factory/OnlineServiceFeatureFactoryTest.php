4edfb4d686e10d6cc9bdab4e447f21042064f086
Felix Becker <becker@sitepark.com>
2025-11-12 15:26:10 +0100

    test: restore OnlineServiceFeatureFactoryTest

--- test/Service/GraphQL/Factory/OnlineServiceFeatureFactoryTest.php
+++ test/Service/GraphQL/Factory/OnlineServiceFeatureFactoryTest.php
@@ -7,11 +7,14 @@ namespace Atoolo\CityGov\Test\Service\GraphQL\Factory;
 use Atoolo\CityGov\Service\GraphQL\Factory\OnlineServiceFeatureFactory;
 use Atoolo\CityGov\Service\GraphQL\Types\OnlineService;
 use Atoolo\CityGov\Service\GraphQL\Types\OnlineServiceFeature;
+use Atoolo\GraphQL\Search\Factory\LinkFactory;
 use Atoolo\GraphQL\Search\Types\Link;
 use Atoolo\Resource\DataBag;
 use Atoolo\Resource\Resource;
 use Atoolo\Resource\ResourceLanguage;
+use Atoolo\Resource\ResourceLoader;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(OnlineServiceFeatureFactory::class)]
@@ -19,13 +22,25 @@ class OnlineServiceFeatureFactoryTest extends TestCase
 {
     private OnlineServiceFeatureFactory $factory;
 
+    private ResourceLoader&MockObject $resourceLoader;
+
+    private LinkFactory&MockObject $linkFactory;
+
     public function setUp(): void
     {
-        $this->factory = new OnlineServiceFeatureFactory();
+        $this->resourceLoader = $this->createMock(ResourceLoader::class);
+        $this->linkFactory = $this->createMock(LinkFactory::class);
+        $this->factory = new OnlineServiceFeatureFactory(
+            $this->resourceLoader,
+            $this->linkFactory,
+        );
     }
 
     public function testCreate()
     {
+        $onlineServiceResource = $this->createResource([]);
+        $onlineServiceResourceUrl =  '/path/to/online/service';
+        $onlineServiceResourceLink = new Link($onlineServiceResourceUrl);
         $resource = $this->createResource([
             'metadata' => [
                 'citygovProduct' => [
@@ -33,18 +48,27 @@ class OnlineServiceFeatureFactoryTest extends TestCase
                         'some' => 'data',
                         'serviceList' => [
                             'items' => [[
-                                'url' => '/path/to/online/service',
+                                'url' => $onlineServiceResourceUrl,
                             ]],
                         ],
                     ],
                 ],
             ],
         ]);
+        $this->resourceLoader
+            ->method('load')
+            ->with($onlineServiceResourceUrl)
+            ->willReturn($onlineServiceResource);
+        $this->linkFactory
+            ->method('create')
+            ->with($onlineServiceResource)
+            ->willReturn($onlineServiceResourceLink);
+
         $onlineServiceFeatures = $this->factory->create($resource);
         $this->assertEquals(
             new OnlineServiceFeature(
                 null,
-                [new OnlineService(new Link('/path/to/online/service'))],
+                [new OnlineService($onlineServiceResourceLink)],
             ),
             $onlineServiceFeatures[0],
         );
