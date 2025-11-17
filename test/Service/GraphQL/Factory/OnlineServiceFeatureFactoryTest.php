f24e08815065cbd1f1601d2447e1a02ac3190a76
Felix Becker <167435367+sitepark-becker@users.noreply.github.com>
2025-11-17 07:56:05 +0100

    feat(OnlineServiceFeature): use linkfactory for online service link (#45)
    * feat(OnlineServiceFeature): use linkfactory for online service link
    
    * test: restore OnlineServiceFeatureFactoryTest
    
    * test: added test to OnlineServiceFeatureFactoryTest

--- test/Service/GraphQL/Factory/OnlineServiceFeatureFactoryTest.php
+++ test/Service/GraphQL/Factory/OnlineServiceFeatureFactoryTest.php
@@ -7,25 +7,45 @@ namespace Atoolo\CityGov\Test\Service\GraphQL\Factory;
 use Atoolo\CityGov\Service\GraphQL\Factory\OnlineServiceFeatureFactory;
 use Atoolo\CityGov\Service\GraphQL\Types\OnlineService;
 use Atoolo\CityGov\Service\GraphQL\Types\OnlineServiceFeature;
+use Atoolo\GraphQL\Search\Factory\LinkFactory;
 use Atoolo\GraphQL\Search\Types\Link;
 use Atoolo\Resource\DataBag;
 use Atoolo\Resource\Resource;
 use Atoolo\Resource\ResourceLanguage;
+use Atoolo\Resource\ResourceLoader;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 
 #[CoversClass(OnlineServiceFeatureFactory::class)]
 class OnlineServiceFeatureFactoryTest extends TestCase
 {
     private OnlineServiceFeatureFactory $factory;
 
+    private ResourceLoader&MockObject $resourceLoader;
+
+    private LoggerInterface&MockObject $logger;
+
+    private LinkFactory&MockObject $linkFactory;
+
     public function setUp(): void
     {
-        $this->factory = new OnlineServiceFeatureFactory();
+        $this->resourceLoader = $this->createMock(ResourceLoader::class);
+        $this->linkFactory = $this->createMock(LinkFactory::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->factory = new OnlineServiceFeatureFactory(
+            $this->resourceLoader,
+            $this->linkFactory,
+        );
+        $this->factory->setLogger($this->logger);
     }
 
     public function testCreate()
     {
+        $onlineServiceResource = $this->createResource([]);
+        $onlineServiceResourceUrl =  '/path/to/online/service';
+        $onlineServiceResourceLink = new Link($onlineServiceResourceUrl);
         $resource = $this->createResource([
             'metadata' => [
                 'citygovProduct' => [
@@ -33,24 +53,33 @@ class OnlineServiceFeatureFactoryTest extends TestCase
                         'some' => 'data',
                         'serviceList' => [
                             'items' => [[
-                                'url' => '/path/to/online/service',
+                                'url' => $onlineServiceResourceUrl,
                             ]],
                         ],
                     ],
                 ],
             ],
         ]);
+        $this->resourceLoader
+            ->method('load')
+            ->with($onlineServiceResourceUrl)
+            ->willReturn($onlineServiceResource);
+        $this->linkFactory
+            ->method('create')
+            ->with($onlineServiceResource)
+            ->willReturn($onlineServiceResourceLink);
+
         $onlineServiceFeatures = $this->factory->create($resource);
         $this->assertEquals(
             new OnlineServiceFeature(
                 null,
-                [new OnlineService(new Link('/path/to/online/service'))],
+                [new OnlineService($onlineServiceResourceLink)],
             ),
             $onlineServiceFeatures[0],
         );
     }
 
-    public function testCreateFail()
+    public function testCreateNoOnlineServices()
     {
         $resource = $this->createResource([
             'metadata' => [
@@ -61,6 +90,31 @@ class OnlineServiceFeatureFactoryTest extends TestCase
         ]);
         $onlineServiceFeatures = $this->factory->create($resource);
         $this->assertEmpty($onlineServiceFeatures);
+    }
+
+    public function testCreateUnknownResource()
+    {
+        $onlineServiceResourceUrl =  '/path/to/online/service';
+        $resource = $this->createResource([
+            'metadata' => [
+                'citygovProduct' => [
+                    'onlineServices' => [
+                        'some' => 'data',
+                        'serviceList' => [
+                            'items' => [[
+                                'url' => $onlineServiceResourceUrl,
+                            ]],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+        $this->resourceLoader
+            ->method('load')
+            ->with($onlineServiceResourceUrl)
+            ->willThrowException(new \Exception());
+        $this->logger->expects($this->once())->method('error');
+        $this->factory->create($resource);
     }
 
     /**
