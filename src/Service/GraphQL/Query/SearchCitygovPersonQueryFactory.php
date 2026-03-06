6dbb6dd723a2d71a7831d73625677148d0310c1a
Felix Becker <167435367+sitepark-becker@users.noreply.github.com>
2026-03-06 15:19:46 +0100

    feat: added sort criteria to person search (#49)

--- src/Service/GraphQL/Query/SearchCitygovPersonQueryFactory.php
+++ src/Service/GraphQL/Query/SearchCitygovPersonQueryFactory.php
@@ -5,15 +5,20 @@ declare(strict_types=1);
 namespace Atoolo\CityGov\Service\GraphQL\Query;
 
 use Atoolo\CityGov\Service\GraphQL\Input\SearchCitygovPersonInput as SearchInput;
+use Atoolo\GraphQL\Search\Query\SortCriteriaFactory;
 use Atoolo\Resource\ResourceLanguage;
 use Atoolo\Search\Dto\Search\Query\QueryOperator;
 use Atoolo\Search\Dto\Search\Query\SearchQuery;
 
 class SearchCitygovPersonQueryFactory
 {
+    private readonly SortCriteriaFactory $sortCriteriaFactory;
+
     public function __construct(
         private readonly CitygovPersonFilterFactory $citygovPersonFilterFactory,
-    ) {}
+    ) {
+        $this->sortCriteriaFactory = new SortCriteriaFactory();
+    }
 
     public function create(SearchInput $input): SearchQuery
     {
@@ -22,7 +27,10 @@ class SearchCitygovPersonQueryFactory
             ResourceLanguage::of($input->lang),
             $input->offset ?? 0,
             $input->limit ?? 10,
-            [],
+            array_map(
+                fn($sort) => $this->sortCriteriaFactory->create($sort),
+                $input->sort ?? [],
+            ),
             $this->citygovPersonFilterFactory->createForCitygovPersonSearch($input),
             [],
             true,
